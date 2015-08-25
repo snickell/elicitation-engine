6870265a0c9670060944d401b620c60488849223
Seth Nickell <snickell@gmail.com>
2015-08-25 16:28:08 -0700

    refactor db access into nzdb.js

---
+++ nzdb.js
@@ -0,0 +1,77 @@
+
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var ncsBuilder = require('node-connection-string-builder');
+
+function connectionStringToConfig(connectionString) {
+  var config = {
+    server: undefined,
+    userName: undefined,
+    password: undefined
+    ,options: {
+      debug: {
+        packet: true,
+        data: true,
+        payload: true,
+        token: false,
+        log: true
+      },
+      database: 'NearZero',
+      encrypt: true // for Azure users
+    }
+  };
+    
+  var builder=new ncsBuilder(connectionString);
+  if (builder.dataSource) {
+    var ds=builder.dataSource.split('\\', 2);
+    config.server=ds[0] || config.server; 
+    var instanceName=ds[1] || config.options.instanceName;
+    if (instanceName)
+      config.options.instanceName=instanceName;
+  }
+  config.userName=builder.userID || config.userName;
+  config.password=builder.password || config.password;
+  config.options.encrypt=builder.encrypt || config.options.encrypt;
+  config.options.database=builder.initialCatalog || config.options.database;  
+  
+  return config;
+}
+
+var NZDB = function (connectionString) {
+  var config = connectionStringToConfig(connectionString)
+  this.connection = new Connection(config);
+
+  this.connection.on('connect', function(err) {
+    console.log("DB Connected");
+  }); 
+}
+
+NZDB.prototype.query = function (query, cb) {
+  var con = this.connection;
+  
+  var rowResults = [];
+    
+  request = new Request(query, function(err, rowCount) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(rowCount + ' rows');
+    }
+
+    cb(err, rowResults);
+  });
+    
+  request.on('row', function(columns) {
+    var result = {};
+    
+    columns.forEach(function(column) {
+      result[column.metadata.colName] = column.value;
+    });
+
+    rowResults.push(result);
+  });
+    
+  con.execSql(request);
+}
+
+module.exports = NZDB;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 
 var express = require('express')
-  , http = require('http')
-  , path = require('path');
+, http = require('http')
+, path = require('path');
 
 var exphbs  = require('express-handlebars');
 
@@ -29,97 +29,24 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-
-var config = {
-  server: undefined,
-  userName: undefined,
-  password: undefined
-  ,options: {
-    debug: {
-      packet: true,
-      data: true,
-      payload: true,
-      token: false,
-      log: true
-    },
-    database: 'NearZero',
-    encrypt: true // for Azure users
-  }
-};
-
 var connectionString = process.env['SQLAZURECONNSTR_DefaultConnection'];
-var ncsBuilder=require('node-connection-string-builder');
-var builder=new ncsBuilder(connectionString);
-if (builder.dataSource) {
-    var ds=builder.dataSource.split('\\', 2);
-    config.server=ds[0] || config.server; 
-    var instanceName=ds[1] || config.options.instanceName;
-    if (instanceName)
-        config.options.instanceName=instanceName;
-}
-config.userName=builder.userID || config.userName;
-config.password=builder.password || config.password;
-config.options.encrypt=builder.encrypt || config.options.encrypt;
-config.options.database=builder.initialCatalog || config.options.database;
+var NZDB = require('./nzdb');
+var db = new NZDB(connectionString);
 
-console.log("Config is", config);
 
-var connection = new Connection(config);
 
-connection.on('connect', function(err) {
-    // If no error, then good to go...
-    executeStatement();
-  }
-);
-
-var dbMessage = "";
-
-function executeStatement() {
-  request = new Request("select name from Discussions", function(err, rowCount) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(rowCount + ' rows');
-    }
-
-    connection.close();
+app.get('/', function (req, res) {  
+  res.render('home', {
+    title: "bumpy"
   });
-
-  request.on('row', function(columns) {
-    columns.forEach(function(column) {
-      if (column.value === null) {
-        console.log('NULL');
-      } else {
-        console.log(column.value);
-        dbMessage += ", " + column.value;
-      }
-    });
-  });
-
-  request.on('done', function(rowCount, more) {
-    console.log(rowCount + ' rows returned');
-  });
-
-  // In SQL Server 2000 you may need: connection.execSqlBatch(request);
-  connection.execSql(request);
-}
-
-app.get('/', function (req, res) {
-    res.render('home', {
-        title: "bumpy"
-    });
 });
 
 app.get('/noodlefactory', function (req, res) {
+  db.query("select * from Discussions", function (err, results) {
     res.render('noodlefactory', {
-        value: "skimpy",
-        cloudProvider: process.env.CloudProvider,
-        thing: dbMessage,
-        test: process.env['SQLAZURECONNSTR_DefaultConnection'],
+      results: JSON.stringify(results[0]),
     });
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
